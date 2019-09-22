d82606674b50242b3ab5a1e0184926b3ae570e09
Enes Aydın <enes.aydin@sahibinden.com>
2019-09-22 23:06:46 +0300

    major update

---
+++ js/enums.js
@@ -0,0 +1,10 @@
+let Kure = {
+    BIREYSEL: "Bireysel",
+    ALISVERIS: "Alisveris",
+    ARAMA: "Arama",
+    FINANS: "Finans",
+    ILAN: "Ilan",
+    KURUMSAL: "Kurumsal",
+    REKLAM: "Reklam"
+};
+
--- js/report.js
+++ js/report.js
@@ -1,6 +1,5 @@
 function getReport(isKureColumnView) {
-    $(document).ready(function () {
-        $('#test_result').DataTable({
+    $('#test_result').DataTable({
             "ajax": "json/TestResults.json",
             "columns": [
                 {"data": "Kure"},
@@ -17,9 +16,10 @@ function getReport(isKureColumnView) {
                     "targets": [0],
                     "visible": isKureColumnView,
                 }
-            ]
-        });
-    });
+            ],
+            select: true,
+        }
+    );
 }
 
 function getDisabledKureColumnWithReport() {
@@ -28,9 +28,27 @@ function getDisabledKureColumnWithReport() {
 
 function filterTable(kure) {
     $(document).ready(function () {
+
         var table = $("#test_result").DataTable();
-        table.columns([0])
-            .search(kure)
-            .draw();
+
+        table.columns([0]).search(kure);
+    });
+}
+
+function writeTestList() {
+    $('#write-test-list').click(function () {
+
+        var selectedItems = $('#test_result').DataTable();
+
+        $.map(selectedItems.rows('.selected').data(), function (item) {
+
+            var node = document.createElement("LI");
+
+            var textnode = document.createTextNode(item.TestClassName.concat(".").concat(item.TestMethod));
+            node.appendChild(textnode);
+
+            document.getElementById("events").appendChild(node);
+        });
+
     });
 }
