bcca4cb6f935c4b5eec14c90ed904478e2136e5b
Enes Aydın <enes.aydin@sahibinden.com>
2019-09-28 13:07:51 +0300

    page up button added, css files fragmentation

--- js/report.js
+++ js/report.js
@@ -95,7 +95,7 @@ function writeTestList() {
             document.getElementById("events").appendChild(node);
         });
 
-        document.querySelector("[class='dt-button']").scrollIntoView({
+        document.getElementById("events").scrollIntoView({
             behavior: 'smooth'
         });
     });
@@ -237,4 +237,20 @@ function setReportInfo() {
             document.querySelector("#reklam-passed > a").textContent = reklamPassedCount;
             document.querySelector("#reklam-failed > a").textContent = reklamFailedCount;
         });
+}
+
+function topScroll() {
+    // ===== Scroll to Top ====
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function () {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0                       // Scroll to top of body
+        }, 500);
+    });
 }
